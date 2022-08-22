97659f476159eaf92b9ef2148eebeb3d15a24553
Greidy Peña <greidykp@gmail.com>
2022-08-22 10:32:23 -0500

    Estructura principal del proyecto

---
+++ src/components/Authors.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Authors = () => {
+  return (
+    <div className="md:w-1/2 lg:w-1/2 mx-5">
+        <h2 className="text-xl font-bold text-indigo-900">Authors</h2>
+    </div>
+  )
+}
+
+export default Authors
---
+++ src/components/Books.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Books = () => {
+  return (
+    <div className="md:w-1/2 lg:w-1/2 mx-5">
+        <h2 className="text-xl font-bold text-indigo-900">Books</h2>
+    </div>
+  )
+}
+
+export default Books
